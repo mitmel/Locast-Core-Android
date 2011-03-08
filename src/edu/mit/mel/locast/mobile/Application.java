9c21817713acc01d5d75a534a0d2afc60c5b28ac
Steve Pomeroy <steve@staticfree.info>
2011-03-08 14:55:03 -0500

    Fixed out-of-memory bug with thumbnail generation.
    Cleaned up classpath.

--- src/edu/mit/mel/locast/mobile/Application.java
+++ src/edu/mit/mel/locast/mobile/Application.java
@@ -2,17 +2,17 @@ package edu.mit.mel.locast.mobile;
 
 /*
  * Copyright (C) 2010 MIT Mobile Experience Lab
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -28,34 +28,35 @@ import com.commonsware.cwac.thumbnail.ThumbnailMessage;
 
 /**
  * Contains some useful extra services that are shared with the rest of the application.
- * 
+ *
  * @author steve
  *
  */
 public class Application extends android.app.Application {
 	private final ThumbnailBus bus = new ThumbnailBus();
 	private SimpleWebImageCache<ThumbnailBus, ThumbnailMessage> imgCache;
-	
-	private WebImageLoader imageLoader; 
-	
+
+	private WebImageLoader imageLoader;
+
 	public SimpleWebImageCache<ThumbnailBus, ThumbnailMessage> getImageCache() {
-		if (imgCache == null){ 
-		    	imgCache = new SimpleWebImageCache<ThumbnailBus, ThumbnailMessage>(getCacheDir(), policy, 101, 
+		if (imgCache == null){
+		    	imgCache = new SimpleWebImageCache<ThumbnailBus, ThumbnailMessage>(getCacheDir(), policy, 101,
 		    			bus);
+		    	imgCache.setMaxSize(300, 300);
 		}
 		return imgCache;
 	}
-	
+
     private final AsyncCache.DiskCachePolicy policy=new AsyncCache.DiskCachePolicy() {
         public boolean eject(File file) {
             return(System.currentTimeMillis()-file.lastModified()>1000*60*60*24*7);
         }
     };
-	
+
 	public ThumbnailBus getThumbnailBus() {
 		return bus;
 	}
-	
+
 	/**
 	 * @return the cached image loader.
 	 */
