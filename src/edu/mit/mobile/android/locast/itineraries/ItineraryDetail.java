25e6d2f6dae1735f9e90415928c85ce03b86dae9
Steve Pomeroy <steve@staticfree.info>
2011-04-04 12:21:24 -0400

    Add cast icons to itinerary detail
    Reset the striped background to 1px lines. The other size was too
    distracting.
    
    Increase shadow on picture frame.

--- src/edu/mit/mobile/android/locast/itineraries/ItineraryDetail.java
+++ src/edu/mit/mobile/android/locast/itineraries/ItineraryDetail.java
@@ -1,16 +1,30 @@
 package edu.mit.mobile.android.locast.itineraries;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.commonsware.cwac.cache.SimpleWebImageCache;
+import com.commonsware.cwac.thumbnail.ThumbnailAdapter;
+import com.commonsware.cwac.thumbnail.ThumbnailBus;
+import com.commonsware.cwac.thumbnail.ThumbnailMessage;
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
 
+import edu.mit.mobile.android.locast.Application;
 import edu.mit.mobile.android.locast.R;
 import edu.mit.mobile.android.locast.casts.CastCursorAdapter;
 import edu.mit.mobile.android.locast.data.Cast;
@@ -19,33 +33,99 @@ public class ItineraryDetail extends MapActivity {
 
 	private MapView mMapView;
 	private MapController mMapController;
+	private ListView mCastView;
+
+	protected SimpleWebImageCache<ThumbnailBus, ThumbnailMessage> imgCache;
 
 	@Override
 	protected void onCreate(Bundle icicle) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
 		super.onCreate(icicle);
 		setContentView(R.layout.itinerary_detail);
 
-		final ListView castView = (ListView)findViewById(R.id.casts);
-		final Cursor casts = managedQuery(Cast.CONTENT_URI, Cast.PROJECTION, null, null, Cast.SORT_ORDER_DEFAULT);
-		final CastCursorAdapter castAdapter = new CastCursorAdapter(this, casts);
+		imgCache = ((Application)getApplication()).getImageCache();
 
-		castView.addHeaderView(getLayoutInflater().inflate(R.layout.itinerary_detail_list_header, castView, false));
+		mCastView = (ListView)findViewById(R.id.casts);
 
-		castView.setAdapter(castAdapter);
+		mCastView.addHeaderView(getLayoutInflater().inflate(R.layout.itinerary_detail_list_header, mCastView, false));
 
 		((TextView)findViewById(R.id.description)).setText(R.string.itinerary_test_text);
 
 		mMapView = (MapView)findViewById(R.id.map);
 		mMapController = mMapView.getController();
 
-		mMapController.setCenter(new GeoPoint(42464753,14230299));
-		mMapController.setZoom(16);
-
+		new ItineraryLoader().execute(Cast.CONTENT_URI);
 	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
+
+	private class ItineraryLoader extends AsyncTask<Uri, Long, Cursor>{
+
+		@Override
+		protected void onPreExecute() {
+			findViewById(R.id.progress).setVisibility(View.VISIBLE);
+		}
+
+		@Override
+		protected Cursor doInBackground(Uri... params) {
+			final Cursor casts = ItineraryDetail.this.managedQuery(params[0], Cast.PROJECTION, null, null, Cast.SORT_ORDER_DEFAULT);
+			return casts;
+		}
+
+		@Override
+		protected void onPostExecute(Cursor casts) {
+			findViewById(R.id.progress).setVisibility(View.INVISIBLE);
+
+			final CastCursorAdapter castAdapter = new CastCursorAdapter(ItineraryDetail.this, casts);
+
+
+			mCastView.setAdapter((new ThumbnailAdapter(ItineraryDetail.this, castAdapter, imgCache, new int[]{R.id.media_thumbnail})));
+
+			final ItineraryOverlay itineraryOverlay = new ItineraryOverlay(ItineraryDetail.this);
+			mMapView.getOverlays().add(itineraryOverlay);
+
+			mMapController.zoomToSpan(itineraryOverlay.getLatSpanE6(), itineraryOverlay.getLonSpanE6());
+			mMapController.setCenter(itineraryOverlay.getCenter());
+		}
+	}
+
+
+
+	private class ItineraryOverlay extends ItemizedOverlay<OverlayItem> {
+		Random r = new Random();
+		ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
+
+		public ItineraryOverlay(Context context) {
+			super(boundCenterBottom(context.getResources().getDrawable(R.drawable.map_marker_user_cast)));
+
+			int lat = 42464753;
+			int lon = 14230299;
+			for (int i = 0; i < 5; i++){
+				lat += r.nextInt(10000) - 5000;
+				lon += r.nextInt(10000) - 5000;
+				items.add(new OverlayItem(new GeoPoint(lat, lon), "foo", "bar"));
+			}
+			populate();
+		}
+
+		@Override
+		protected OverlayItem createItem(int i) {
+			return items.get(i);
+
+		}
+
+
+		@Override
+		public int size() {
+
+			return items.size();
+		}
+
+	}
+
+
 }
