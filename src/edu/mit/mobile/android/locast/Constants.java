c39c3673c30951f80a9e3a0953b06bff1c85a4c7
Ary Borenszweig <ary@esperanto.org.ar>
2012-01-13 17:03:13 -0300

    Put Constants#DEBUG to false

--- src/edu/mit/mobile/android/locast/Constants.java
+++ src/edu/mit/mobile/android/locast/Constants.java
@@ -22,7 +22,7 @@ public class Constants {
 	/**
 	 * General debugging flag. This needs to be a constant so that the compiler will remove any debugging code.
 	 */
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 	/**
 	 * If true, the user can create casts.
 	 */
