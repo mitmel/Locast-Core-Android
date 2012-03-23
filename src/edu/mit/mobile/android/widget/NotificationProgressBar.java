27eaf96a29caa15df87eb8886f79a944a2878a77
Steve Pomeroy <steve@staticfree.info>
2012-03-23 15:01:10 -0400

    Optimized NotificationProgressBar layouts

--- src/edu/mit/mobile/android/widget/NotificationProgressBar.java
+++ src/edu/mit/mobile/android/widget/NotificationProgressBar.java
@@ -3,46 +3,53 @@ package edu.mit.mobile.android.widget;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import edu.mit.mobile.android.locast.ver2.R;
 
-public class NotificationProgressBar extends LinearLayout {
-	String textID;
-	private ProgressBar mProgressBar;
-	private TextView mTextView;
-	
+/**
+ * Shows either an empty view or a progress bar.
+ *
+ */
+public class NotificationProgressBar extends FrameLayout {
+	private int emptyTextId;
+	private final ProgressBar mProgressBar;
+	private final TextView mTextView;
+
 	public NotificationProgressBar(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		init(attrs);
-		View v=null;
-		LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if(inflater != null){       
-            v=inflater.inflate(R.layout.notification_progress_bar,this);
-        }
-		mProgressBar=(ProgressBar)findViewById(R.id.progressBar);
-		mTextView=(TextView)findViewById(R.id.emptyTextView);
-		mTextView.setText(textID);
+
+		View.inflate(context, R.layout.notification_progress_bar, this);
+
+		mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+		mTextView = (TextView) findViewById(R.id.emptyTextView);
+		if (emptyTextId != 0) {
+			mTextView.setText(emptyTextId);
+		}
 	}
+
 	public NotificationProgressBar(Context context) {
 		super(context);
+
 		View.inflate(context, R.layout.notification_progress_bar, this);
-		mProgressBar=(ProgressBar)findViewById(R.id.progressBar);
-		mTextView=(TextView)findViewById(R.id.emptyTextView);
+
+		mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+		mTextView = (TextView) findViewById(R.id.emptyTextView);
 	}
 
 	private void init(AttributeSet attrs) {
-		TypedArray a = getContext().obtainStyledAttributes(attrs,
+		final TypedArray a = getContext().obtainStyledAttributes(attrs,
 				R.styleable.NotificationProgressBar);
-		textID = a.getString(R.styleable.NotificationProgressBar_android_text);
+		emptyTextId = a.getResourceId(R.styleable.NotificationProgressBar_android_text, 0);
 		a.recycle();
 	}
-	public void showProgressBar(boolean b){
-		mProgressBar.setVisibility((b?View.VISIBLE:View.GONE));
-		mTextView.setVisibility(((!b)?View.VISIBLE:View.GONE));
+
+	public void showProgressBar(boolean b) {
+		mProgressBar.setVisibility((b ? View.VISIBLE : View.GONE));
+		mTextView.setVisibility(((!b) ? View.VISIBLE : View.GONE));
 		postInvalidate();
 	}
 }
